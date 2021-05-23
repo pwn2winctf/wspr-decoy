2ce32ecdd0824d6232753bb78536e3fe135da8a0
Paulo Matias <matias@ufscar.br>
2021-05-23 12:41:49 -0300

    Add impulsive noise simulation

--- solver/spinal_test.py
+++ solver/spinal_test.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
 from hashlib import sha3_224
-from random import gauss
+from random import gauss, randint
 from spinal import encode, decode
 from math import sqrt
 import os
 
-num_frames = 6*48
+num_frames = 6*12
 
 signal_power = 0.25
-noise_power = 128*signal_power
+noise_power = signal_power
 noise_std_dev = sqrt(noise_power)
+impulsive_noise_power = 128*signal_power
+impulsive_noise_std_dev = sqrt(noise_power)
+impulsive_noise_len = 20
 
 M_len = 5*32
 M = ''.join(str(os.urandom(1)[0] & 1) for _ in range(M_len))
@@ -22,7 +25,10 @@ frames = []
 for _ in range(num_frames):
     seed = sha3_224(os.urandom(224//8)).digest()
     symbols = encode(seed, M, 162)
-    symbols = [sym + gauss(0, noise_std_dev) for sym in symbols]
+    start = randint(0, 162-impulsive_noise_len)
+    symbols = [sym + gauss(0,
+        impulsive_noise_std_dev if (i>=start and i<=start+impulsive_noise_len)
+        else noise_std_dev) for i, sym in enumerate(symbols)]
     seeds.append(seed)
     frames.append(symbols)
 
