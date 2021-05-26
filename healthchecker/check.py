3ce9f5cef2537c121a4a680c21b438dfa443bc91
Paulo Matias <matias@ufscar.br>
2021-05-26 15:01:13 -0300

    change healthchecker

--- healthchecker/check.py
+++ healthchecker/check.py
@@ -1,28 +1,25 @@
 #!/usr/bin/env python3
 import websocket
+import traceback
 import _thread as thread
 import json
 import time
 
-result = -1
-
-
 def on_message(ws, message):
-    global result
     message = json.loads(message)
     if isinstance(message, dict) and message.get('type') == 'update':
         for spot in message['value']:
             if spot['callsign'] == 'PU2UID':
                 if spot['mode'] != 'WSPR':
-                    result = 0
+                    ws._result = 0
                 elif spot['band'] != '40m':
-                    result = 0
+                    ws._result = 0
                 elif spot['location']['locator'] != 'GG68':
-                    result = 0
+                    ws._result = 0
                 elif time.time() - spot['lastseen']/1000. > 630:
-                    result = -1
+                    ws._result = -1
                 else:
-                    result = 1
+                    ws._result = 1
                 ws.close()
 
 
@@ -36,12 +33,16 @@ def on_open(ws):
 
 
 def solve(chall_addr='pu2uid.duckdns.org', chall_port=443):
-    global result
-    ws = websocket.WebSocketApp("wss://{}:{}/ws/".format(chall_addr, chall_port),
-                                on_open=on_open,
-                                on_message=on_message)
-    ws.run_forever()
-    return result
+    try:
+        ws = websocket.WebSocketApp("wss://{}:{}/ws/".format(chall_addr, chall_port),
+                                    on_open=on_open,
+                                    on_message=on_message)
+        ws._result = -1
+        ws.run_forever()
+        return ws._result
+    except:
+        traceback.print_exc()
+        return -1
 
 
 if __name__ == "__main__":
